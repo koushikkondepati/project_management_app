1de59e9ae1fb120173e05bf4364dfd9fa2bb1d88
koushikkondepati <koushikkondepati4255@gmail.com>
2020-07-13 09:43:36 +0530

    added project-card using Js

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,14 @@ function toggleMenu() {
         toggle.style.display = "none";
     }
 }
+showProjectCards();
+function showProjectCards() {
+        var template = '<div class="project-card">'+
+            'Project 1'+
+            '<ul>'+
+                '<li>Task one</li>'+
+                '<li>Task two</li>'+
+            '</ul>'+
+        '</div>';
+        document.getElementById('project-list').innerHTML = template;
+}
