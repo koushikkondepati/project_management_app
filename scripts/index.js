e31749a15dfb3b8faa10fc92707b4946fb2179aa
koushikkondepati <koushikkondepati4255@gmail.com>
2020-07-13 09:02:25 +0530

    added toggle functionality

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var toggle = document.getElementById('board-menu');
+    if ( toggle.style.display == "none") {
+        toggle.style.display = "";
+    } else {
+        toggle.style.display = "none";
+    }
+}
